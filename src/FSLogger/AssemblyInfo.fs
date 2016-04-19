b8674175adf66766fdb4ef0af18f441ef4d68c24
varon <varon@github.com>
2016-04-19 13:37:06 +0200

    1.1 release

--- src/FSLogger/AssemblyInfo.fs
+++ src/FSLogger/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSLogger")>]
 [<assembly: AssemblyProductAttribute("FSLogger")>]
 [<assembly: AssemblyDescriptionAttribute("F#ing simple logger for F#.")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("1.1")>]
+[<assembly: AssemblyFileVersionAttribute("1.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "1.1"
