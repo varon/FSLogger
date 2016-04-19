dc7c13a26e2ec03b218d633857a82e9f0e4e7652
varon <varon@github.com>
2016-04-19 11:32:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# F#ing Simple Logger
+
+An rich, simple, and efficient purely functional micro logging library for F#.
---
+++ src/FSLogger/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FSLogger")>]
+[<assembly: AssemblyProductAttribute("FSLogger")>]
+[<assembly: AssemblyDescriptionAttribute("F#ing simple logger for F#.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
