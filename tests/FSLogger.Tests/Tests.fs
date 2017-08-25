51db169414ef55032d767cbabd8ab18470879fc7
varon <varon@github.com>
2017-08-25 14:19:39 +0200

    Add test for ShortString messages

--- tests/FSLogger.Tests/Tests.fs
+++ tests/FSLogger.Tests/Tests.fs
@@ -2,6 +2,25 @@ module FSLogger.Tests
 
 open FSLogger
 open Xunit
-[<Fact>]
-let ``Placeholder`` () =
-  true
+open FsCheck.Xunit
+open System
+
+
+
+[<Property>]
+let ``LogEntry ShortString contains then last value``(s:string) =
+    if not <| String.IsNullOrEmpty s then
+        let s = s.Replace("/", "").Replace("\\", "")
+        let le = new LogEntry(LogLevel.Info,DateTime.Now,"Test/" + s, "msg")
+        le.ShortString.Contains(s)
+    else true
+
+[<Property>]
+let ``LogEntry ShortString does not contain first bit of then path value``(s:string) =
+    if not <| (String.IsNullOrEmpty s || s.Contains "Test") then
+        let s = s.Replace("/", "").Replace("\\", "")
+        let le = new LogEntry(LogLevel.Info,DateTime.Now,"Test/ABC", "msg")
+        not <| le.ShortString.Contains("Test")
+    else
+        true
+    
